b34723c2118fc754e6d72a0a7446085ec1730ffe
Habib ul Rehman <mhabibulrehman@gmail.com>
2020-04-21 21:06:54 +0500

    write basic operations for topic

---
+++ IslamByAge.Infrastructure/Data/IslamByAgeDBContext.cs
@@ -0,0 +1,71 @@
+﻿using Google.Cloud.Firestore;
+using IslamByAge.Infrastructure.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IslamByAge.Infrastructure.Data
+{
+    public class IslamByAgeFirestoreDB
+    {
+        private readonly FirestoreDb db;
+        public IslamByAgeFirestoreDB()
+        {
+            db = FirestoreDb.Create();
+        }
+        public CollectionReference Topics
+        {
+            get { return db.Collection("Topics"); }
+        }
+        public async Task<bool> InsertTopic(Topic topic)
+        {
+            try
+            {
+                var docRef = Topics.Document();
+                await docRef.SetAsync(topic);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public async Task<bool> UpdateTopic(Topic topic)
+        {
+            try
+            {
+                var docRef = Topics.Document(topic.Id.ToString());
+                await docRef.SetAsync(topic, SetOptions.Overwrite);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public async Task<Topic> GetTopicById(string id)
+        {
+            var docRef = Topics.Document(id);
+            var snapshot = await docRef.GetSnapshotAsync();
+            if (snapshot.Exists)
+            {
+                return snapshot.ConvertTo<Topic>();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public async Task<List<Topic>> GetTopics()
+        {
+            var snapshot = await Topics.GetSnapshotAsync();
+            var topics = new List<Topic>();
+            foreach (var item in snapshot.Documents)
+            {
+                topics.Add(item.ConvertTo<Topic>()); 
+            }
+            return topics;
+        }
+    }
+}
---
+++ IslamByAge.Infrastructure/DomainModels/Topic.cs
@@ -0,0 +1,22 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IslamByAge.Infrastructure.DomainModels
+{
+    [FirestoreData]
+    public class Topic
+    {
+        [FirestoreProperty]
+        public int Id { get; set; }
+        [FirestoreProperty]
+        public string Title { get; set; }
+        [FirestoreProperty]
+        public string Description { get; set; }
+        [FirestoreProperty]
+        public string Body { get; set; }
+        [FirestoreProperty]
+        public bool IsActive { get; set; }
+    }
+}

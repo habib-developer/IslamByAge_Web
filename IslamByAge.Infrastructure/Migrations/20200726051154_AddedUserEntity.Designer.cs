d405eb1c60cfaf327edae861155bd209a9c46dde
Muhammad Sufyan <sufyan004@outlook.com>
2020-07-29 16:29:40 +0500

    add migration for user

--- IslamByAge.Infrastructure/Migrations/20200726051154_AddedUserEntity.Designer.cs
+++ IslamByAge.Infrastructure/Migrations/20200726051154_AddedUserEntity.Designer.cs
@@ -4,14 +4,16 @@ using IslamByAge.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IslamByAge.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200726051154_AddedUserEntity")]
+    partial class AddedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IslamByAge.Infrastructure/Migrations/20200726051154_AddedUserEntity.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IslamByAge.Infrastructure.Migrations
+{
+    public partial class AddedUserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Topics",
+                type: "NVARCHAR(MAX)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Categories",
+                type: "NVARCHAR(MAX)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Topics",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(MAX)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(MAX)",
+                oldNullable: true);
+        }
+    }
+}
